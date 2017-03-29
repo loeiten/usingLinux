da01d7dcfffa45aa97cb1366fcf24290f6349a27
loeiten <michael.l.magnussen@gmail.com>
2017-03-29 15:09:35 +0200

    Added cmake install script

---
+++ installScripts/cmake.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Installs the master branch of BOUT-dev
+CMAKE_MAJOR=3
+CMAKE_MINOR=7
+CMAKE_PATCH=2
+
+# exit on error
+set -e
+
+CMAKE_MAJOR_MINOR=${CMAKE_MAJOR}.${CMAKE_MINOR}
+CMAKE_VERSION=${CMAKE_MAJOR_MINOR}.${CMAKE_PATCH}
+
+echo -e "\nDownloading CMAKE\n"
+cd $HOME
+mkdir -p local
+mkdir -p install
+cd install
+wget --no-check-certificate http://cmake.org/files/v${CMAKE_MAJOR_MINOR}/cmake-${CMAKE_VERSION}.tar.gz
+tar -xzvf cmake-${CMAKE_VERSION}.tar.gz
+cd cmake-${CMAKE_VERSION}
+echo -e "\nConfiguring\n"
+./bootstrap --prefix=$HOME/local
+echo -e "\nMaking\n"
+make
+make install
+echo -e "\n\n\nSuccess!"
