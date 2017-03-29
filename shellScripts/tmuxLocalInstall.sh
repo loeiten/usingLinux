b74d9ea0edb50775a7d2179741003a56579ef90a
loeiten <michael.l.magnussen@gmail.com>
2017-03-29 12:09:15 +0200

    Modifications to tmuxLocalInstall

--- shellScripts/tmuxLocalInstall.sh
+++ shellScripts/tmuxLocalInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This based on https://gist.github.com/brandondrew/3915551, which is a fork of
-# https://gist.github.com/ryin/3106801. See also
+# https://gist.github.com/ryin/3106801
 
-TMUX_VERSION="2.2"
-LIBEVENT_VERSION="2.0.22"
+TMUX_VERSION="2.3"
+LIBEVENT_VERSION="2.1.8"
 NCURSES_VERSION="6.0"
 
 # Script for installing tmux on systems where you don't have root access.
@@ -19,15 +19,19 @@ mkdir -p $HOME/local $HOME/tmux_tmp
 cd $HOME/tmux_tmp
 
 # download source files for tmux, libevent, and ncurses
+echo -e "\n\n\n\n\nDownloading tmux...\n\n"
 wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
-wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}-stable/libevent-${LIBEVENT_VERSION}-stable.tar.gz
-wget http://ftp.gnu.org/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz
+echo -e "\n\n\n\n\n Downloading libevent...\n\n"
+wget -O libevent-${LIBEVENT_VERSION}-stable.tar.gz https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}-stable/libevent-${LIBEVENT_VERSION}-stable.tar.gz
+echo -e "\n\n\n\n\n Downloading ncurses...\n\n"
+wget -O ncurses-${NCURSES_VERSION}.tar.gz http://ftp.gnu.org/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz
 
 # extract files, configure, and compile
 
 ############
 # libevent #
 ############
+echo -e "Installing libevent..."
 tar xvzf libevent-${LIBEVENT_VERSION}-stable.tar.gz
 cd libevent-${LIBEVENT_VERSION}-stable
 ./configure --prefix=$HOME/local --disable-shared
@@ -38,16 +42,17 @@ cd ..
 ############
 # ncurses  #
 ############
+echo -e "Installing ncurses..."
 tar xvzf ncurses-${NCURSES_VERSION}.tar.gz
 cd ncurses-${NCURSES_VERSION}
 ./configure --prefix=$HOME/local
 make
 make install
-cd ..
 
 ############
 # tmux     #
 ############
+echo -e "Installing tmux..."
 tar xvzf tmux-${TMUX_VERSION}.tar.gz
 cd tmux-${TMUX_VERSION}
 ./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
@@ -56,6 +61,7 @@ cp tmux $HOME/local/bin
 cd ..
 
 # cleanup
+echo -e "Clean-up..."
 rm -rf $HOME/tmux_tmp
 
 echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
