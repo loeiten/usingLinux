fab7bf1b8451d11f9442fbf7621434773413388e
loeiten <michael.l.magnussen@gmail.com>
2017-03-29 12:21:47 +0200

    Added gcc local install

---
+++ shellScripts/gccLocalInstall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This based on http://luiarthur.github.io/gccinstall
+
+# NOTE: The installation usually takes long time
+
+# exit on error
+set -e
+
+VERSION="6.1.0"
+
+mkdir -p $HOME/install
+cd $HOME/install
+
+echo -e "\n\n\n\n\n Downloading gcc version ${VERSION}\n"
+wget ftp://ftp.fu-berlin.de/unix/languages/gcc/releases/gcc-${VERSION}/gcc-${VERSION}.tar.gz
+
+echo -e "\n\n\n\n\n Extracting\n"
+tar xzf gcc-${VERSION}.tar.gz
+cd gcc-${VERSION}
+
+echo -e "\n\n\n\n\n Downloading prerequisites\n"
+./contrib/download_prerequisites
+cd ..
+mkdir -p objdir
+cd objdir
+
+echo -e "\n\n\n\n\n Configuring gcc\n"
+$PWD/../gcc-{$VERSION}/configure --prefix=$HOME/gcc-${VERSION}
+
+
+echo -e "\n\n\n\n\n Making\n"
+make
+
+echo -e "\n\n\n\n\n Make install\n"
+make install
+
+echo -e "\n\n\n\n\n Success!\n"
