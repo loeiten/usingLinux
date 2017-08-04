951daff856a7949557ae68fec73401cfed96a97b
Michael Løiten <michael.l.magnussen@gmail.com>
2017-08-04 15:04:13 +0200

    Added nasm to ffmpeg install

--- installScripts/ffmpegInstall.sh
+++ installScripts/ffmpegInstall.sh
@@ -3,13 +3,14 @@
 # Installs ffmpeg
 FFMPEG_VERSION="3.1.4"
 YASM_VERSION="1.3.0"
+NASM_VERSION="2.13.01"
 
 # exit on error
 set -e
 
 # Install ffmpeg
 echo -e "\n\n\nInstalling ffmpeg\n\n\n"
-echo -e "\n\n\nNow building yasm\n\n\n"
+echo -e "\n\n\nNow building nasm\n\n\n"
 cd $HOME
 mkdir -p local
 cd local
@@ -17,6 +18,17 @@ mkdir -p examples
 cd ..
 mkdir -p install
 cd install
+wget http://www.nasm.us/pub/nasm/releasebuilds/${NASM_VERSION}/nasm-${NASM_VERSION}.tar.gz
+tar -xvzf nasm-${NASM_VERSION}.tar.gz
+cd nasm-${NASM_VERSION}
+# : is the no-op command
+make clean || :
+./configure --prefix=$HOME/local
+make
+make install
+
+echo -e "\n\n\nNow building yasm\n\n\n"
+cd ..
 wget http://www.tortall.net/projects/yasm/releases/yasm-${YASM_VERSION}.tar.gz
 tar -xvzf yasm-${YASM_VERSION}.tar.gz
 cd yasm-${YASM_VERSION}
@@ -31,6 +43,8 @@ echo -e "\n\n\nNow building x264\n\n\n"
 cd ..
 git clone git://git.videolan.org/x264.git
 cd x264/
+# : is the no-op command
+make clean || :
 ./configure --prefix=$HOME/local --enable-static --enable-shared
 make
 make install
